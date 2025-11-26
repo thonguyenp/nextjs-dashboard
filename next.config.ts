21f4df19661d2278de97f4c15d991b2344076476
thonguyenp <nphuoctho2406@gmail.com>
2025-11-26 15:23:41 +0700

    Commit đầu tiên cho phần nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://192.168.56.1:3000",  // IP bạn dùng để truy cập
+    "http://localhost:3000",     // localhost
+  ],
 };
 
 export default nextConfig;
